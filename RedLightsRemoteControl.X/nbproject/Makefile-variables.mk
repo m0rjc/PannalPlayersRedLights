8b00bd12bbd4c17f3b281833ba313b462d70177a
Richard Corfield <rcorfield@financialforce.com>
2018-01-27 09:45:10 +0000

    Adding in project files

---
+++ RedLightsRemoteControl.X/nbproject/Makefile-variables.mk
@@ -0,0 +1,13 @@
+#
+# Generated - do not edit!
+#
+# NOCDDL
+#
+CND_BASEDIR=`pwd`
+# default configuration
+CND_ARTIFACT_DIR_default=dist/default/production
+CND_ARTIFACT_NAME_default=RedLightsRemoteControl.X.production.hex
+CND_ARTIFACT_PATH_default=dist/default/production/RedLightsRemoteControl.X.production.hex
+CND_PACKAGE_DIR_default=${CND_DISTDIR}/default/package
+CND_PACKAGE_NAME_default=redlightsremotecontrol.x.tar
+CND_PACKAGE_PATH_default=${CND_DISTDIR}/default/package/redlightsremotecontrol.x.tar
